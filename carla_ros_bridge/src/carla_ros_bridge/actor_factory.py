ad11aace7e1a74cbe3782b3ac34f44e12c43e68a
Joey_myUbuntu <gz285639@gmail.com>
2026-04-17 12:32:06 +0800

    bug fixes

--- carla_ros_bridge/src/carla_ros_bridge/actor_factory.py
+++ carla_ros_bridge/src/carla_ros_bridge/actor_factory.py
@@ -388,7 +388,10 @@ class ActorFactory(object):
                 if carla_actor.type_id.endswith("sensor.lidar.ray_cast"):
                     actor = Lidar(uid, name, parent, spawn_pose, self.node,
                                   carla_actor, self.sync_mode)
-                elif carla_actor.type_id.startswith("sensor.lidar.doppler"):
+                elif (
+                    carla_actor.type_id.startswith("sensor.lidar.doppler")
+                    or carla_actor.type_id.startswith("sensor.lidar.fmcw")
+                ):
                     actor = DopplerLidar(uid, name, parent, spawn_pose, self.node,
                                        carla_actor, self.sync_mode)     
                 elif carla_actor.type_id.endswith(
